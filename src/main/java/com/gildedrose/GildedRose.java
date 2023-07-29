47dac22e32b24aa23c065669a8b8ee6614e5cd25
YousefNaeemHakim <yousefnaeem22@gmail.com>
2023-07-29 17:52:03 +0300

    Adding the Conjured requirement

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -11,25 +11,37 @@ class GildedRose {
         for (Item item : items) {
             ItemUpdater itemUpdater = getItemUpdater(item);
             itemUpdater.updateQuality(item);
+
+            if (item.quality < 0) {
+                item.quality = 0;
+            }
+
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros") && item.quality > 50) {
+                item.quality = 50;
+            }
         }
     }
-    
+
     private ItemUpdater getItemUpdater(Item item) {
+        // Determine the appropriate updater for each item based on its name.
         if (item.name.equals("Aged Brie")) {
             return new AgedBrieUpdater();
         } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             return new BackstagePassUpdater();
         } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
             return new SulfurasUpdater();
+        } else if (item.name.equals("Conjured Mana Cake")) { // Corrected the item name check
+            return new ConjuredItemUpdater();
         } else {
+            // Default updater
             return new RegularItemUpdater();
         }
     }
-    
+
     interface ItemUpdater {
         void updateQuality(Item item);
     }
-    
+
     class AgedBrieUpdater implements ItemUpdater {
         @Override
         public void updateQuality(Item item) {
@@ -40,7 +52,7 @@ class GildedRose {
             }
         }
     }
-    
+
     class BackstagePassUpdater implements ItemUpdater {
         @Override
         public void updateQuality(Item item) {
@@ -57,14 +69,27 @@ class GildedRose {
             }
         }
     }
-    
+
     class SulfurasUpdater implements ItemUpdater {
         @Override
         public void updateQuality(Item item) {
             // Sulfuras never changes in quality or sellIn
         }
     }
-    
+
+    class ConjuredItemUpdater implements ItemUpdater {
+        @Override
+        public void updateQuality(Item item) {
+            decreaseQuality(item);
+            decreaseQuality(item);
+            decreaseSellIn(item);
+            if (item.sellIn < 0) {
+                decreaseQuality(item);
+                decreaseQuality(item);
+            }
+        }
+    }
+
     class RegularItemUpdater implements ItemUpdater {
         @Override
         public void updateQuality(Item item) {
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,6 +8,7 @@ public class GildedRoseTest {
 
     @Test
     void testUpdateQualityForRegularItem() {
+        // Test that a regular item's quality and sellIn decrease by one.
         Item[] items = new Item[] { new Item("Regular Item", 5, 10) };
         GildedRose app = new GildedRose(items);
 
@@ -19,6 +20,7 @@ public class GildedRoseTest {
 
     @Test
     void testUpdateQualityForAgedBrie() {
+        // Test that Aged Brie's quality increases by one while sellIn decreases by one.
         Item[] items = new Item[] { new Item("Aged Brie", 5, 10) };
         GildedRose app = new GildedRose(items);
 
@@ -30,6 +32,7 @@ public class GildedRoseTest {
 
     @Test
     void testUpdateQualityForBackstagePasses() {
+        // Test Backstage Passes quality increases as sellIn approaches.
         Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20) };
         GildedRose app = new GildedRose(items);
 
@@ -41,8 +44,10 @@ public class GildedRoseTest {
 
     @Test
     void testUpdateQualityForSulfuras() {
+        // Test that Sulfuras quality and sellIn remain unchanged.
         Item[] items = new Item[] {
                 new Item("Sulfuras, Hand of Ragnaros", 5, 80),
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80)
         };
         GildedRose app = new GildedRose(items);
 
@@ -50,14 +55,29 @@ public class GildedRoseTest {
 
         assertEquals(5, app.items[0].sellIn);
         assertEquals(80, app.items[0].quality);
+
+    }
+
+    @Test
+    void testUpdateQualityForConjuredItem() {
+        // Test that Conjured items degrade in quality twice as fast as regular items.
+        Item[] items = new Item[] { new Item("Conjured Mana Cake", 3, 6) };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(2, app.items[0].sellIn);
+        assertEquals(4, app.items[0].quality);
     }
 
     @Test
     void testQualityNeverNegative() {
+        // Test that the quality of any item never goes negative.
         Item[] items = new Item[] {
+                new Item("Regular Item", 5, 0),
                 new Item("Aged Brie", 5, 0),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 5, 0),
-                new Item("Sulfuras, Hand of Ragnaros", 5, 0),
+                new Item("Conjured Mana Cake", 5, 0)
         };
         GildedRose app = new GildedRose(items);
 
@@ -70,9 +90,11 @@ public class GildedRoseTest {
 
     @Test
     void testQualityNeverExceeds50() {
+        // Test that the quality of Aged Brie, Backstage Passes, and Conjured items never exceeds 50.
         Item[] items = new Item[] {
                 new Item("Aged Brie", 5, 50),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 5, 50),
+                new Item("Conjured Mana Cake", 5, 50)
         };
         GildedRose app = new GildedRose(items);
 
@@ -85,9 +107,12 @@ public class GildedRoseTest {
 
     @Test
     void testSellInPassed() {
+        // Test that the quality degrades twice as fast after the sellIn date has passed.
         Item[] items = new Item[] {
+                new Item("Regular Item", 0, 10),
                 new Item("Aged Brie", 0, 10),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10),
+                new Item("Conjured Mana Cake", 0, 10)
         };
         GildedRose app = new GildedRose(items);
 
